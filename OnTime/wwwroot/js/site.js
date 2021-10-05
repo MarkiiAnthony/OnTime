fc59802f6642ee19921691fdd96b2eb71da2a6e7
techguymarkii <mark@atlasdevct.com>
2021-10-05 16:10:29 -0400

    NewLayout

--- OnTime/wwwroot/js/site.js
+++ OnTime/wwwroot/js/site.js
@@ -3,4 +3,23 @@
 
 // Write your JavaScript code.
 
-/*Numberic Keypad*/
+(function ($) {
+
+	"use strict";
+
+	var fullHeight = function () {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function () {
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+	});
+
+})(jQuery);
+
