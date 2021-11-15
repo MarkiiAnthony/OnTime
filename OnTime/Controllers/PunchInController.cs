97a67db80075363c67279bee1df4c25580c3de31
techguymarkii <mark@atlasdevct.com>
2021-11-14 20:16:20 -0500

    Timer

--- OnTime/Controllers/PunchInController.cs
+++ OnTime/Controllers/PunchInController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OnTime.Interfaces;
 using OnTime.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OnTime.Controllers
@@ -13,11 +16,13 @@ namespace OnTime.Controllers
     
         public class PunchInController : Controller
     {
+        private readonly IStopwatch _StopwatchService;
         private readonly ApplicationDbContext _db;
         UserManager<ApplicationUser> _UserManager;
 
-        public PunchInController( ApplicationDbContext db, UserManager<ApplicationUser> UserManager)
+        public PunchInController( ApplicationDbContext db, UserManager<ApplicationUser> UserManager,IStopwatch stopwatchService)
         {
+            _StopwatchService = stopwatchService;
              _db = db;
             _UserManager = UserManager;
         }
@@ -29,21 +34,22 @@ namespace OnTime.Controllers
         [HttpPost]
         public IActionResult PunchTool()
         {
-
-
+         
 
             var punchTool = new PunchClockModel
             {
+                 
 
-                PunchedIn = true,
+                 PunchedIn = true,
                 PunchedOut = false,
                 PunchDateTime = DateTime.Now,
                 Employee = User.FindFirstValue(ClaimTypes.Name),
 
             };
+            _StopwatchService.StartTimer();
 
-                _db.Add(punchTool);
-                _db.SaveChanges();
+            _db.Add(punchTool);
+            _db.SaveChanges();
            
 
          
---
+++ OnTime/Services/StopwatchService.cs
@@ -0,0 +1,32 @@
+﻿using OnTime.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnTime.Services
+{
+    public class StopwatchService : IStopwatch
+    {
+        public TimeSpan StartTimer()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Thread.Sleep(10000);
+
+            return stopWatch.Elapsed;
+        }
+
+        public TimeSpan StopTimer()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Stop();
+            Thread.Sleep(10000);
+
+            return stopWatch.Elapsed; 
+
+        }
+    }
+}
