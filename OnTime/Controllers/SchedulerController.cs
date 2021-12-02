150ec8ae3ee4b63ea9d4bf609e1385c31d4a3353
techguymarkii <mark@atlasdevct.com>
2021-12-01 23:59:35 -0500

    GoogleAPI

--- OnTime/Controllers/SchedulerController.cs
+++ OnTime/Controllers/SchedulerController.cs
@@ -1,23 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using OnTime.Interfaces;
 using OnTime.Models;
 using OnTime.Models.Viewmodels;
 using OnTime.Utility;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OnTime.Controllers
 {
     public class SchedulerController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public List<string> GoogleEvents = new List<string>();
         private readonly ISchedulingService _schedulingService;
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
 
-
-        public SchedulerController(ISchedulingService schedulingService)
+        public SchedulerController(ISchedulingService schedulingService,IWebHostEnvironment webHostEnvironment)
             {
             _schedulingService = schedulingService;
+            _webHostEnvironment = webHostEnvironment;
             }
         public IActionResult Index()
         {
@@ -26,14 +38,62 @@ namespace OnTime.Controllers
 
         public IActionResult ScheduleManager()
         {
-            ViewBag.Duration = Helper.GetShiftLength();
-            ViewBag.UserList = _schedulingService.GetAllUsers();
+            SchedulerApi();
+            ViewBag.EventList = GoogleEvents;
             
             
            
             
 
             return View();
+        }
+
+        public void SchedulerApi()
+        {
+            UserCredential credential;
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+            string path = Path.Combine(contentRootPath, "Credential.json");
+            using (var stream =
+                new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    GoogleEvents.Add(eventItem.Summary);
+                }
+            }
+           
         }
 
        
